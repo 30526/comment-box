f2947e90f52326c98f437c6cfb0a468004307982
30526 <taiburrahamanapu@gmail.com>
2025-09-01 19:30:59 +0600

    added js step one

---
+++ js/comment.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-comment')
+    .addEventListener('click', function () {
+        // step 01 get the text from the text box
+        const textBox = document.getElementById('text-Box')
+        const comment = textBox.value
+    }
+    )
